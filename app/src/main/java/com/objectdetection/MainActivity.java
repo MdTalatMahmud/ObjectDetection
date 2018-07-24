23df6968c79eca3d0d65c9d31478cb939a4d18ed
samdi18 <41270492+samdi18@users.noreply.github.com>
2018-07-25 03:25:11 +0600

    OpenCV Camera Module added

--- app/src/main/java/com/objectdetection/MainActivity.java
+++ app/src/main/java/com/objectdetection/MainActivity.java
@@ -1,7 +1,10 @@
 package com.objectdetection;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +22,19 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        // Button to call OpenCV Camera Activity
+        Button cameraInit = (Button) findViewById(R.id.cameraInit);
+        cameraInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),OpenCVCamera.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
